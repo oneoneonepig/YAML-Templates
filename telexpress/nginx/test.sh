c483d2a4a562f5f7ab6077b98469e3b17982514d
oneoneonepig <peihuachu1112@gmail.com>
2019-08-08 19:28:33 +0800

    update files

--- telexpress/nginx/test.sh
+++ telexpress/nginx/test.sh
@@ -6,6 +6,6 @@ INGRESS_IP=$(kubectl get svc -n istio-system istio-ingressgateway -o=jsonpath='{
 # Interval 1.5 seconds, timeout 1 second
 
 watch -n 2 "\
-echo && curl -s -m 0.5 http://$INGRESS_IP/v1/labels.html && \
-echo '\n\n' && curl -m 0.5 -s http://$INGRESS_IP/v2/labels.html && \
-echo '\n\n' && curl -m 0.5 -s http://$INGRESS_IP/v3/labels.html"
+echo ; curl -s -m 0.5 http://$INGRESS_IP/v1/labels.html ; \
+echo '\n\n' ; curl -m 0.5 -s http://$INGRESS_IP/v2/labels.html ; \
+echo '\n\n' ; curl -m 0.5 -s http://$INGRESS_IP/v3/labels.html"
